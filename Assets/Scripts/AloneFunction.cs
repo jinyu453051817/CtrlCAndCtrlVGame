57e2862957d568ca7ef4ec9d64e38c6782614eaa
jinyu453051817 <43825921+jinyu453051817@users.noreply.github.com>
2021-03-16 21:18:00 +0800

    对话框
    对话框

---
+++ Assets/Scripts/AloneFunction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AloneFunction : MonoBehaviour
+{
+    public Text upText;
+    public GameObject rightPrefab;
+    public GameObject content;
+
+    public void ShowDiaInRight()
+    {
+        print("upText:" + upText.text);
+        GameObject go = Instantiate(rightPrefab, content.transform);
+        go.transform.Find("copyotherText").GetComponent<InputField>().text = upText.text;
+    }
+}
