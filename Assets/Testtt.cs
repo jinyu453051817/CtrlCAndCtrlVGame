27271dced1196b65ae1663ef8d333be1d3e9919f
jinyu453051817 <43825921+jinyu453051817@users.noreply.github.com>
2022-10-20 13:47:38 +0800

    最后提交
    最后提交

---
+++ Assets/Testtt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using
+using System.Runtime.CompilerServices;
+
+public class Testtt : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private bool test;
+
+    public extern bool enabled {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        get {
+        };
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        set
+        { }
+         }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enabled = GetComponent<SpriteRenderer>().enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
